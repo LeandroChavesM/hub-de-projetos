bb4259d1c03a0f6dd0a4f534d3fcd0dd5fe75c7d
LeandroChavesM <leandrochavesmata@hotmail.com>
2025-10-19 12:15:06 -0300

    updating files

--- projects/detona-ralph/src/scripts/engine.js
+++ projects/detona-ralph/src/scripts/engine.js
@@ -12,12 +12,30 @@ const state = {
     result: 0,
     currentTime: 60,
     lifes: 3,
+    isGameActive: false,
   },
   actions: {
     timerId: setInterval(randomSquare, 1000),
     countDownTimerId: setInterval(countDown, 1000),
   },
 };
+
+function resetGame() {
+  state.values.result = 0;
+  state.values.currentTime = 60;
+  state.values.lifes = 3;
+  state.view.score.textContent = state.values.result;
+  state.view.timeLeft.textContent = state.values.currentTime;
+  state.view.lifes.textContent = state.values.lifes;
+  state.actions.timerId = setInterval(randomSquare, state.values.gameVelocity);
+  state.actions.countDownTimerId = setInterval(countDown, 1000);
+}
+
+function disableGame() {
+  state.view.squares.forEach((square) => {
+    square.replaceWith(square.cloneNode(true)); // remove todos os event listeners
+  });
+}
 
 function countDown() {
   state.values.currentTime--;
@@ -27,6 +45,12 @@ function countDown() {
     clearInterval(state.actions.countDownTimerId);
     clearInterval(state.actions.timerId);
     alert("Seu jogo acabou! Seu resultado foi: " + state.values.result);
+
+    if (confirm("Você quer jogar denovo?")) {
+      resetGame();
+    } else {
+      disableGame();
+    }
   }
 }
 
@@ -39,6 +63,7 @@ function playSound() {
 function randomSquare() {
   state.view.squares.forEach((square) => {
     square.classList.remove("enemy");
+    state.values.isGameActive = true;
   });
 
   let randomNumber = Math.floor(Math.random() * 9);
@@ -54,6 +79,7 @@ function currentLifes() {
 function addListenerHitBox() {
   state.view.squares.forEach((square) => {
     square.addEventListener("mousedown", () => {
+      if (!state.values.isGameActive) return; // ignora cliques antes do jogo começar
       if (square.id === state.values.hitPosition) {
         playSound();
         state.values.result++;
